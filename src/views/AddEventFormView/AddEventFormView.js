239d667c3886aafcd6c4c7846bb551c2ec801735
kubzer0 <jakub.gryka@gmail.com>
2018-11-22 16:58:03 +0100

    changed component to class, added 1st textfild and state

--- src/views/AddEventFormView/AddEventFormView.js
+++ src/views/AddEventFormView/AddEventFormView.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import TextField from 'material-ui/TextField'
 
-const AddEventForm = (props) => (
-    <div></div>
-)
+class AddEventForm extends React.Component {
+
+    state = {
+        eventName: ''
+    }
+
+    render() {
+        return (
+            <TextField
+                type="text"
+                value={this.state.eventName}
+                floatingLabelText="Enter event name"
+                onChange={(event, newVal) => this.setState({ eventName: newVal })}
+            />
+        )
+    }
+}
+
 
 export default AddEventForm
