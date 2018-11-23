7b391488b75df429f00103f8e072674b7e38e772
kubzer0 <jakub.gryka@gmail.com>
2018-11-23 16:25:39 +0100

    All textFields added, they update state in AddEventForm for respective property

--- src/views/AddEventFormView/AddEventFormView.js
+++ src/views/AddEventFormView/AddEventFormView.js
@@ -4,17 +4,42 @@ import TextField from 'material-ui/TextField'
 class AddEventForm extends React.Component {
 
     state = {
-        eventName: ''
+        eventName: '',
+        category: '',
+        date: '',
+        city: '',
+        street: ''
     }
 
     render() {
         return (
-            <TextField
-                type="text"
-                value={this.state.eventName}
-                floatingLabelText="Enter event name"
-                onChange={(event, newVal) => this.setState({ eventName: newVal })}
-            />
+            <div>
+                <TextField
+                    type="text"
+                    floatingLabelText="Enter event name"
+                    onChange={(event, newVal) => this.setState({ eventName: newVal })}
+                />
+                <TextField
+                    type="text"
+                    floatingLabelText="Enter event category"
+                    onChange={(event, newVal) => this.setState({ category: newVal })}
+                />
+                <TextField
+                    type="text"
+                    floatingLabelText="Enter event date"
+                    onChange={(event, newVal) => this.setState({ date: newVal })}
+                />
+                <TextField
+                    type="text"
+                    floatingLabelText="Enter city"
+                    onChange={(event, newVal) => this.setState({ city: newVal })}
+                />
+                <TextField
+                    type="text"
+                    floatingLabelText="Enter street name"
+                    onChange={(event, newVal) => this.setState({ street: newVal })}
+                />
+            </div>
         )
     }
 }
