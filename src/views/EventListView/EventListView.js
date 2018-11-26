93e2d5dcb33b39e64f073b051ea8efd5b0e70d26
Jarpen94 <kwiatowskijarek@o2.pl>
2018-11-26 16:54:57 +0100

    creating and modyfing handlers

--- src/views/EventListView/EventListView.js
+++ src/views/EventListView/EventListView.js
@@ -1,16 +1,36 @@
 import React from 'react'
 
+
 class EventListView extends React.Component {
-constructor (props) {
-    super(props)
-    state  = {
-        dataSource: demoList
-    }
-}
-}
 
-    
-    <div></div>
-)
+  state = {
+    category: '',
+    date: '',
+    city: '',
+    street: '',
+    eventName: ''
+  }
 
+  handleCategoryChange = (e, index, value) => this.setState({ category: value });
+  handleDateChange = (e, value) => this.setState({ date: value });
+  handleCityChange = (e, value) => this.setState({ city: value });
+  handleStreetChange = (e, value) => this.setState({ street: value });
+  handleEventNameChange = (e, value) => this.setState({ eventName: value })
+
+  render() {
+    return (
+      <div>
+<List
+handleCategoryChange={this.handleCategoryChange}
+handleDateChangeChange={this.handleDateChange}
+handleCityChange={this.handleCityChange}
+handleStreetChange={this.handleStreetChange}
+handleEventNameChange={this.handleEventNameChange}
+/>
+
+
+      </div>
+    )
+  }
+}
 export default EventListView
