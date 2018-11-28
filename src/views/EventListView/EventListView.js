2684cc904c98102c15cc25418c7fe9b9d4954800
Jarpen94 <kwiatkowskijarek@o2.pl>
2018-11-28 21:23:15 +0100

    working with the repo

--- src/views/EventListView/EventListView.js
+++ src/views/EventListView/EventListView.js
@@ -1,76 +1,74 @@
 import React, { Component } from 'react'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Paper from 'material-ui/Paper';
 import { GridList, GridTile } from 'material-ui/GridList';
 import IconButton from 'material-ui/IconButton';
 import Subheader from 'material-ui/Subheader';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 import EventListViewPaper from './EventListViewPaper'
 
-class EventListView extends React.Component{
+class EventListView extends React.Component {
     state = {
         cols: 2
     }
 
 
-componentWillMount(){
-    const width = window.innerWidth
+    componentWillMount() {
+        const width = window.innerWidth
 
-    if (width < 1000) {
-        this.setState({
-            cols:2
-        })
-    } else {
-        this.setState({
-            cols:4
-        })
+        if (width < 1000) {
+            this.setState({
+                cols: 2
+            })
+        } else {
+            this.setState({
+                cols: 4
+            })
+        }
     }
-}
 
-render(){
-    return (
-        <div>
-            <EventListViewPaper>
-                <div style={styles.root}>
-                <GridList
-                cellHeight={180}
-                style={styles.gridList}
-                cols={this.state.cols}
-            >
-    
-                <Subheader>December</Subheader>
-                {tilesData.map((tile) => (
-                    <GridTile
-                        key={tile.img}
-                        title={tile.title}
-                        subtitle={<span>by <b>{tile.author}</b></span>}
-                        actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
-                    >
-                        <img src={tile.img} />
-                    </GridTile>
-                ))}
-            </GridList>
+    render() {
+        return (
+            <div>
+                <EventListViewPaper>
+                    <div style={styles.root}>
+                        <GridList
+                            cellHeight={180}
+                            style={styles.gridList}
+                            cols={this.state.cols}
+                        >
+
+                            <Subheader>Event</Subheader>
+                            {tilesData.map((tile) => (
+                                <GridTile
+                                    key={tile.img}
+                                    title={tile.title}
+                                    subtitle={<span>by <b>{tile.author}</b></span>}
+                                    actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+                                >
+                                    <img src={tile.img} />
+                                </GridTile>
+                            ))}
+                        </GridList>
+                    </div>
+                </EventListViewPaper>
             </div>
-            </EventListViewPaper>
-        </div>
-    );
-}
+        );
+    }
 }
 const tilesData = [
     {
-      img: 'images/grid-list/00-52-29-429_640.jpg',
-      title: 'Breakfast',
-      author: 'jill111',
+        img: 'images/grid-list/00-52-29-429_640.jpg',
+        title: 'Breakfast',
+        author: 'jill111',
     },
     {
-      img: 'images/grid-list/burger-827309_640.jpg',
-      title: 'Tasty burger',
-      author: 'pashminu',
+        img: 'images/grid-list/burger-827309_640.jpg',
+        title: 'Tasty burger',
+        author: 'pashminu',
     },
     {
-      img: 'images/grid-list/camera-813814_640.jpg',
-      title: 'Camera',
-      author: 'Danson67',
+        img: 'images/grid-list/camera-813814_640.jpg',
+        title: 'Camera',
+        author: 'Danson67',
     },
 ]
 
