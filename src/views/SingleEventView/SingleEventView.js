62986a534e94214e95ba9bc1cee9374943ec6a52
marcingal <marcin921125@gmail.com>
2018-11-29 17:03:18 +0100

    added single-event-view

--- src/views/SingleEventView/SingleEventView.js
+++ src/views/SingleEventView/SingleEventView.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import Paper from 'material-ui/Paper'
+import { Grid, Row, Col } from 'react-flexbox-grid'
 
-const SingleEventView = (props) => (
-    <div>
 
-    </div>
-)
+class SingleEventView extends React.Component {
+
+    render() {
+        return (
+            <Paper>
+                <Grid>
+                    <Row>
+                        <Col>
+                            <h1>naglowek1</h1>
+                        </Col>
+                    </Row>
+                </Grid>
+            </Paper>
+        )
+    }
+}
 
 export default SingleEventView
