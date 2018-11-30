b911160b0458c04c68aca00bbe84880bdb155755
marcingal <marcin921125@gmail.com>
2018-11-30 16:53:45 +0100

    working on attribute list

--- src/views/SingleEventView/SingleEventView.js
+++ src/views/SingleEventView/SingleEventView.js
@@ -2,8 +2,12 @@ import React from 'react'
 import Paper from 'material-ui/Paper'
 import { Grid, Row, Col } from 'react-flexbox-grid'
 
-
 class SingleEventView extends React.Component {
+
+    state = {
+        viewportWidth: window.innerWidth,
+        data: null
+    }
 
     render() {
         return (
@@ -11,7 +15,17 @@ class SingleEventView extends React.Component {
                 <Grid>
                     <Row>
                         <Col>
-                            <h1>naglowek1</h1>
+                            <h1>Event Name:</h1>
+                            <h2>Event Category:</h2>
+                            <h3>Overview:</h3>
+                            <h3>Overview:</h3>
+                        </Col>
+                        <Col>
+                            <img
+                                alt={'photoExample'}
+                                src={'https://ketstatic.cdn.ket.org/wp_transfer/images/NAAT/NAAT__003506.4832838.512x288.jpg'}
+                                style={{ maxWidth: '100%' }}
+                            />
                         </Col>
                     </Row>
                 </Grid>
