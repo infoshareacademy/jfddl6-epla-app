d9144de26cd01af10472b8e7a978a66d50c3f15b
marcingal <marcin921125@gmail.com>
2018-12-02 12:28:02 +0100

    adding new list elements

--- src/views/SingleEventView/SingleEventView.js
+++ src/views/SingleEventView/SingleEventView.js
@@ -1,7 +1,40 @@
 import React from 'react'
+import Paper from 'material-ui/Paper'
+import { Grid, Row, Col } from 'react-flexbox-grid'
 
-const SingleEventView = (props) => (
-    <div></div>
-)
+class SingleEventView extends React.Component {
+
+    state = {
+        viewportWidth: window.innerWidth,
+        data: null
+    }
+
+    render() {
+        return (
+            <Paper>
+                <Grid>
+                    <Row>
+                        <Col xs={12} s={6} md={6}>
+                            <h1>Event Name: Polowanie na żaby</h1>
+                            <h3>Event Category: cultural</h3>
+                            <h3>Date:</h3>
+                            <h3>Numbers of participants:</h3>
+                            <h3>Add to favourites: </h3>
+                        </Col>
+                        <Col xs={12} s={6} md={6}>
+                            <img
+                                alt={'photoExample'}
+                                src={'https://ketstatic.cdn.ket.org/wp_transfer/images/NAAT/NAAT__003506.4832838.512x288.jpg'}
+                                style={{
+                                    maxWidth: '100%',
+                                }}
+                            />
+                        </Col>
+                    </Row>
+                </Grid>
+            </Paper>
+        )
+    }
+}
 
 export default SingleEventView
