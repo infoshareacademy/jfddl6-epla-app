d7a272a15cf75ca0f6696620c7db1534d287bf63
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-12-13 18:11:51 +0100

    formatted code

--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ import ReactDOM from 'react-dom'
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import { Provider } from 'react-redux'
-import {store} from './store'
+import { store } from './store'
 
 import './index.css'
 import App from './App'
-/*import eplaTheme from './style'*/
 
 ReactDOM.render(
     <Provider store={store}>
-        <MuiThemeProvider /*muiTheme={eplaTheme}*/>
+        <MuiThemeProvider>
             <App />
         </MuiThemeProvider>
     </Provider>,
